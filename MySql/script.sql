594927500c50fdf70a1c8c8fa0e19b2d8d37dacc
juvenalfonseca <50221345+juvenalfonseca@users.noreply.github.com>
2022-03-15 18:21:49 -0300

    Add files via upload
    MySql - Como modelar um banco de controle de séries assistidas

---
+++ MySql/script.sql
@@ -0,0 +1,7 @@
+create table series (id integer, nome varchar(100), lancamento integer);
+
+insert into series (id, nome, lancamento) values (1, 'Spartacus', 2010);
+insert into series (id, nome, lancamento) values (2, 'Prison Break', 2005);
+insert into series (id, nome, lancamento) values (3, 'La casa de papel', 2017);
+
+select * from series;
